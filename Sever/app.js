1cab2fc4c1733900f1e3910de2e24eda8a2131c8
Sidit Srivastava <siditsrivastava84@gmail.com>
2023-08-28 11:45:19 +0530

    file

--- Sever/app.js
+++ Sever/app.js
@@ -14,6 +14,6 @@ app.use(bodyParser.json());
 app.use(require("./route"));
 app.use(express.json());
 console.log("sidit Srivastava");
-app.listen(PORT, () => {
+app.listen(4000, () => {
   console.log("sever runn in the localhost 4000");
 });
